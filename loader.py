4e6e5e6a3e9d18432122924483173b2cd38ed59e
Hafiz Arham <137191768+Arham786Pk@users.noreply.github.com>
2025-07-29 13:46:10 +0500

    loader.py

--- loader.py
+++ loader.py
@@ -1,6 +1,6 @@
 import os
 from typing import List
-from langchain.document_loaders import (
+from langchain_community.document_loaders import (
     PyPDFLoader,
     UnstructuredWordDocumentLoader,
     TextLoader,
